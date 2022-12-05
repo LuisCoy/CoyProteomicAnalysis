4b32b564a0221c72edb2ad1009d7475e50f4da68
LuisCoy <111523453+LuisCoy@users.noreply.github.com>
2022-12-05 17:58:21 +0000

    Delete open_csv_files_in_globalenv.R

--- open_csv_files_in_globalenv.R
+++
@@ -1,7 +0,0 @@
-open_csv_files_in_globalenv <- function(file_path){ 
-  #grab file name without path or extension
-  export_name <- basename(file_path) %>%
-    file_path_sans_ext()
-  #read csv file and save as the name1 in the global environment 
-  assign(export_name, read_csv(file_path), envir = .GlobalEnv)
-}
